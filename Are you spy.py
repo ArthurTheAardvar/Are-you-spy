45640b297d8b16a93d3988384e36344f0f959443
ArthurTheAardvar <114256031+ArthurTheAardvar@users.noreply.github.com>
2024-02-08 14:53:55 -0700

    a

---
+++ Are you spy.py
@@ -0,0 +1,28 @@
+cases = int(input(""))
+
+for i in range (cases):
+    line = (input("").lower())
+    sep = []
+    sep = line.split("|")
+
+    g = sep[0]
+    k = list(g)
+
+    p = sep[1]
+    n = list(p)
+
+    duplicat_exist = True
+
+    for n in range(len(p)):
+        if p[n].isalpha(): # if the string utilizes alphabetical letters
+            for _ in range(len(g)):
+                if p[n] == g[_]:
+                    break
+            else: #else being if it uses anything else but letters. Ex: numbers, commas ect.
+                duplicat_exist = False
+    if duplicat_exist == True:
+        print("That's my secret contact!")
+    else:
+        print("You're not a secret agent!")
+                
+            
